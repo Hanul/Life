c96abfb7837f2e7deac2165aa28742562bf27138
Hanul <hanul@hanul.me>
2016-05-03 22:38:24 +0900

    겜창인생 -> Life

--- Life.js
+++ Life.js
@@ -2,21 +2,20 @@ require(process.env.UPPERCASE_PATH + '/BOOT.js');
 
 BOOT({
 	BROWSER_CONFIG : {
-		beforeUnloadMessage : '페이지를 이동하려 합니다. <확인>을 누르면 새로고침되오니, 작성중인 글이 있으신 경우엔 <취소>를 눌러주세요!!\n\n* 실시간 서버가 재시작 되었거나 인터넷이 재연결 된 것일 수 있습니다.'
+		beforeUnloadMessage : '페이지를 이동하려 합니다. <확인>을 누르면 새로고침되오니, 작성중인 글이 있으신 경우엔 <취소>를 눌러주시기 바랍니다.\n\n* 실시간 서버가 재시작 되었거나 인터넷이 재연결 된 것일 수 있습니다.'
 	},
 	CONFIG : {
 		isDevMode : true,
 		defaultBoxName : 'Life',
-		title : '겜창인생',
-		description : '게임을 창작하는 아름다운 인생..',
+		title : 'Life CMS',
 		webServerPort : 8410
 	},
 	NODE_CONFIG : {
 		maxUploadFileMB : 200,
 		isNotUsingCPUClustering : true,
-		dbName : 'GCLife-test',
+		dbName : 'Life-test',
 		Life : {
-			email : 'gclife.net@gmail.com',
+			email : 'test@test.test',
 			emailPassword : 'test'
 		}
 	}
--- README.md
+++ README.md
@@ -1,12 +1,10 @@
-# gclife.net
-겜창인생 사이트 솤슼콬듴
-
-![ScreenShot](https://raw.githubusercontent.com/Hanul/gclife.net/master/well-being.gif)
-
-[UPPERCASE](http://UPPERCASE.IO)와 [NSP](http://nsp.js.org)를 기반으로 제작.
+# Life
+Life는 쉽게 게시판 형 사이트를 구축할 수 있는 [UPPERCASE](http://UPPERCASE.IO)와 [NSP](http://nsp.js.org) 기반의 CMS(Content Management System)입니다.
 
 ## 라이센스
 [MIT](LICENSE)
 
 ## 작성자
 [Young Jae Sim](https://github.com/Hanul)
+
+![ScreenShot](https://raw.githubusercontent.com/Hanul/Life/master/well-being.gif)
